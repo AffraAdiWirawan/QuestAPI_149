a11ae4f8aea0ef26af66d505a41cdc329d468b5d
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-12-30 06:10:19 +0700

    Membuat agar backend kepanggil pertaama ada getmahasiswa dan getmahasiswabynim

---
+++ app/src/main/java/com/umy/pam_api/service_api/MahasiswaService.kt
@@ -0,0 +1,25 @@
+package com.umy.pam_api.service_api
+
+import com.umy.pam_api.model.Mahasiswa
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface MahasiswaService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+    @GET("bacamahasiswa.php")
+    suspend fun getMahasiswa(): List<Mahasiswa>
+
+    @GET("baca1mahasiswa.php/{nim}")
+    suspend fun getMahasiswaByNim(@Query("nim")nim: String): Mahasiswa
+
+
+}
