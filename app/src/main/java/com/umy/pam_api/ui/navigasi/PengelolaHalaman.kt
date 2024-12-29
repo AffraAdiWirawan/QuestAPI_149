11de1278699719e0a657561a15fb57ddae93589d
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-12-30 06:44:12 +0700

    Membuat Pengelola Halaman, dimana setiap halaman dipanggil disini

---
+++ app/src/main/java/com/umy/pam_api/ui/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,82 @@
+package com.umy.pam_api.ui.navigasi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.umy.pam_api.ui.view.DestinasiDetail
+import com.umy.pam_api.ui.view.DestinasiUpdate
+import com.umy.pam_api.ui.view.DetailScreen
+import com.umy.pam_api.ui.view.EntryMhsScreen
+import com.umy.pam_api.ui.view.HomeScreen
+import com.umy.pam_api.ui.view.UpdateScreen
+
+@Composable
+fun PengelolaHalaman(
+    modifier : Modifier = Modifier,
+    navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(route = DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                }
+            )
+        }
+        composable(route = DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+        composable(
+            DestinasiDetail.routeWithArg,
+            arguments = listOf(
+                navArgument(DestinasiDetail.NIM){
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val nim = it.arguments?.getString(DestinasiDetail.NIM)
+            nim?.let {
+                DetailScreen(
+                    navigateBack = {
+                        navController.navigate(DestinasiHome.route) {
+                            popUpTo(DestinasiHome.route) {
+                                inclusive = true
+                            }
+                        }
+                    },
+                    navigateToEdit =  {
+                        navController.navigate("${DestinasiUpdate.route}/$it")
+                    }
+                )
+            }
+        }
+        composable(DestinasiUpdate.routesWithArg, arguments = listOf(navArgument(DestinasiDetail.NIM){
+            type = NavType.StringType
+        }
+        )
+        ){
+            val nim = it.arguments?.getString(DestinasiUpdate.NIM)
+            nim?.let { nim ->
+                UpdateScreen(
+                    onBack = { navController.popBackStack() },
+                    onNavigate = { navController.popBackStack() }
+                )
+            }
+        }
+    }
+}
