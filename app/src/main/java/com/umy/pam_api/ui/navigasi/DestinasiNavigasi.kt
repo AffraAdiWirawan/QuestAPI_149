08a660fdade0e670e96bb5cfab60c18c7893fce8
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-12-30 06:12:19 +0700

    Membuat destinasi home dan entry saja

---
+++ app/src/main/java/com/umy/pam_api/ui/navigasi/DestinasiNavigasi.kt
@@ -0,0 +1,16 @@
+package com.umy.pam_api.ui.navigasi
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
